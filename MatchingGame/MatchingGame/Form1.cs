94ba7b10fc70fe148c4c6c5eb4e8a68cf1186622
wlsgur073 <wlsgur073@gmail.com>
2025-07-07 14:40:24 +0900

    add the click event to reveal the icon

--- MatchingGame/MatchingGame/Form1.cs
+++ MatchingGame/MatchingGame/Form1.cs
@@ -15,6 +15,62 @@ namespace MatchingGame
         public Form1()
         {
             InitializeComponent();
+            AssignIconsToSquares();
+        }
+
+        // Use this Random object to choose random icons for the squares
+        Random random = new Random();
+
+        // Each of these letters is an interesting icon
+        // in the Webdings font,
+        // and each icon appears twice in this list
+        List<string> icons = new List<string>()
+        {
+            "!", "!", "N", "N", ",", ",", "k", "k",
+            "b", "b", "v", "v", "w", "w", "z", "z"
+        };
+
+        /// <summary>
+        /// Assign each icon from the list of icons to a random square
+        /// </summary>
+        private void AssignIconsToSquares()
+        {
+            // The TableLayoutPanel has 16 labels,
+            // and the icon list has 16 icons,
+            // so an icon is pulled at random from the list
+            // and added to each label
+            foreach (Control control in tableLayoutPanel1.Controls)
+            {
+                Label iconLable = control as Label;
+                if (iconLable != null)
+                {
+                    int randomNumber = random.Next(icons.Count);
+                    iconLable.Text = icons[randomNumber];
+                    iconLable.ForeColor = iconLable.BackColor; // Hide the icon
+                    icons.RemoveAt(randomNumber); // Remove the icon so it can't be reused
+                }
+            }
+        }
+
+        /// <summary>
+        /// Every label's Click event is handled by this event handler
+        /// </summary>
+        /// <param name="sender">The label that was clicked</param>
+        /// <param name="e"></param>
+        private void label1_Click(object sender, EventArgs e)
+        {
+            Label clickedLabel = sender as Label;
+
+            if (clickedLabel != null)
+            {
+                // If the clicked label is black, the player clicked
+                // an icon that's already been revealed --
+                // ignore the click
+                if (clickedLabel.ForeColor == Color.Black)
+                    return;
+
+                clickedLabel.ForeColor = Color.Black;
+            }
         }
     }
 }
