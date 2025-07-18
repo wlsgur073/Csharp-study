ffb396e3790223cfd531fda61d99438499f7fdf6
wlsgur073 <wlsgur073@gmail.com>
2025-07-19 08:46:45 +0900

    simple tcp networking code

---
+++ Networkprogramming/Examples/Client01.cs
@@ -0,0 +1,35 @@
+﻿using System.Net.Sockets;
+using System.Text;
+
+namespace Networkprogramming.Examples
+{
+    internal class Client01 : IRunnable
+    {
+        public string Name => "tcp-client";
+        public string Description => "TcpClient로 메시지 전송 예제";
+        public void Run() 
+        {
+            TcpClient client = new TcpClient("localhost", 7);
+            NetworkStream ns = client.GetStream();
+            Console.WriteLine("=== TcpClientExample 클라이언트 실행 ===");
+            byte[] buffer = new byte[1024];
+            byte[] sendMessage = Encoding.ASCII.GetBytes("Hello, Server!");
+            ns.Write(sendMessage, 0, sendMessage.Length);
+            int totalCount = 0, readCount = 0;
+
+            while (totalCount < sendMessage.Length)
+            {
+                readCount = ns.Read(buffer, 0, buffer.Length);
+                if (readCount == 0) break; // 서버가 연결을 종료한 경우
+                totalCount += readCount;
+
+                string recvMessage = Encoding.ASCII.GetString(buffer);
+                Console.WriteLine(recvMessage);
+            }
+
+            Console.WriteLine($"받은 바이트 수: {readCount}, 총 바이트 수: {totalCount}");
+            ns.Close();
+            client.Close();
+        }
+    }
+}
---
+++ Networkprogramming/Examples/TcpListener01.cs
@@ -0,0 +1,40 @@
+﻿using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace Networkprogramming.Examples
+{
+    internal class TcpListener01 : IRunnable
+    {
+        public string Name => "tcp_listener";
+        public string Description => "TCP 리스너를 생성하고 클라이언트 연결을 기다립니다.";
+
+        public void Run()
+        {
+            TcpListener tcpListener = new TcpListener(IPAddress.Any, 7);
+            tcpListener.Start();
+            byte[] Buffer = new byte[1024];
+            int totalBytesCount = 0, readByteCount = 0;
+
+            Console.WriteLine("=== TcpListenerExample 서버 실행 ===");
+
+            TcpClient tcpClient = tcpListener.AcceptTcpClient();
+            NetworkStream ns = tcpClient.GetStream();
+
+            while(true)
+            {
+                readByteCount = ns.Read(Buffer, 0, Buffer.Length);
+                if (readByteCount == 0) break; // 클라이언트가 연결을 종료한 경우
+
+                totalBytesCount += readByteCount;
+                ns.Write(Buffer, 0, readByteCount); // 클라이언트에게 받은 데이터를 그대로 돌려줌
+                Console.WriteLine($"받은 바이트 수: {readByteCount}, 총 바이트 수: {totalBytesCount}");
+                Console.WriteLine(Encoding.ASCII.GetString(Buffer));
+            }
+
+            ns.Close();
+            tcpClient.Close();
+            tcpListener.Stop();
+        }
+    }
+}
